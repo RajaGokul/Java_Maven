0cb91d9ee59031adbffdd3d7a43e92865cda6e2c
RajaGokul <rajagokul11@gmail.com>
2026-06-27 19:09:38 +0530

    Updating code to align with Maven project

---
+++ src/main/java/example/app.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Maven Build Successful ");
+        System.out.println(" Welcome to Jenkins CI/CD ");
+        System.out.println("=================================");
+
+    }
+
+}
